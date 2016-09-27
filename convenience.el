403d627b1badd1cdfdf7793d306bbeea930cb6c1
inaimathi <leo.zovic@gmail.com>
2016-09-27 18:29:38 -0400

    Fix obsolete name
    - Use cl-flet instead of flet
    - Remove trailing whitespace on a few things

--- convenience.el
+++ convenience.el
@@ -3,7 +3,7 @@
 ;;; lisp basics
 (defun flash-region (start end &optional timeout)
   "Temporarily highlight region from START to END. Ripped bleeding from SLIME."
-  (let ((overlay (make-overlay start end))) 
+  (let ((overlay (make-overlay start end)))
     (overlay-put overlay 'face 'secondary-selection)
     (run-with-timer (or timeout 0.2) nil 'delete-overlay overlay)))
 
@@ -33,7 +33,7 @@
   (= (aref path 0) ?.))
 
 (defun list-subdirectories (path)
-  (let ((all (mapcar 
+  (let ((all (mapcar
 	      (lambda (name) (concat (file-name-as-directory path) name))
 	      (remove-if #'starts-with-dot-p (directory-files path)))))
     (remove-if-not #'file-directory-p all)))
@@ -59,7 +59,7 @@
 
 (defmacro hooks (mode-name/s function)
   (assert (or (symbolp mode-name/s) (listp mode-name/s)))
-  (flet ((to-hook (name) (intern (format "%s-mode-hook" name))))
+  (cl-flet ((to-hook (name) (intern (format "%s-mode-hook" name))))
     (if (symbolp mode-name/s)
 	`(add-hook ',(to-hook mode-name/s) ,function)
       (let ((fn (gensym)))
@@ -69,17 +69,17 @@
 
 (defmacro by-ext (extension/s mode)
   (assert (or (stringp extension/s) (listp extension/s)))
-  (flet ((to-reg (ext) (format "\\.%s$" ext)))
+  (cl-flet ((to-reg (ext) (format "\\.%s$" ext)))
     (if (stringp extension/s)
 	`(add-to-list 'auto-mode-alist '(,(to-reg extension/s) . ,mode))
       `(progn ,@(loop for ext in extension/s
-		      collect `(add-to-list 'auto-mode-alist 
+		      collect `(add-to-list 'auto-mode-alist
 					    '(,(to-reg ext) . ,mode)))))))
 
 (defmacro global-mode (mode-name) ;;shortcut for globalizing a minor mode (since I do it more than once)
   (let ((g-name (make-symbol (concat "global-" (symbol-name mode-name)))))
-    `(progn 
-       (define-globalized-minor-mode ,g-name 
+    `(progn
+       (define-globalized-minor-mode ,g-name
 	   ,mode-name
 	 (lambda () (,mode-name t)))
        (,g-name t))))
@@ -104,11 +104,10 @@
 (defun file-nameify-string (a-string)
   (replace-regexp-in-string
    " +" "-"
-   (replace-regexp-in-string 
+   (replace-regexp-in-string
     "[',`~_\"]" ""
-    (replace-regexp-in-string 
-     "part[ 	]\\([0-9]+\\)" "p\\1" 
-     (downcase a-string))))) 
+    (replace-regexp-in-string
+     "part[ 	]\\([0-9]+\\)" "p\\1"
+     (downcase a-string)))))
 
 (provide 'convenience)
-
