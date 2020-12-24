d4cf81da2385297934a46e97a5f257d838119b54
inaimathi <leo.zovic@gmail.com>
2020-12-24 11:50:14 -0500

    Add droplet subdirectory

---
+++ droplet/setup.sh
@@ -0,0 +1,10 @@
+## TODO
+#  - langnostic
+#  - static
+#  - asdf-archives
+#  - certbot
+#  - pronounce
+#  - kicktracker
+#  - lispwiki
+
+## Set all the above up, but realistically, clean out static and decomission everything other than langnostic, certbot, pronounce and kicktracker
