ba0600b2028fe8f4b2d51786f8fd9f8670e35088
inaimathi <leo.zovic@gmail.com>
2023-10-06 18:25:59 -0400

    Remove some unnecessary guix steps and add some core modules

--- machine-root-setup.sh
+++ machine-root-setup.sh
@@ -10,7 +10,6 @@ fi
 if [ ! -f checkpoints/002-root.check ]
 then
    echo "   Installing guix..."
-   gpg --keyserver pool.sks-keyservers.net --recv-keys 3CE464558A84FDC69DB40CFB090B11993D9AEBB5
    wget https://git.savannah.gnu.org/cgit/guix.git/plain/etc/guix-install.sh
    sh guix-install.sh
    touch checkpoints/002-root.check
@@ -19,18 +18,19 @@ fi
 if [ ! -f checkpoints/003-root.check ]
 then
    echo "   Installing basics that don't work out of guix..."
-   apt-get install pmount pacpl mtp-tools libblas-dev liblapack-dev gnupg gnupg-agent python3-pip slock
+   apt-get install pmount pacpl mtp-tools libblas-dev liblapack-dev gnupg gnupg-agent python3-pip slock pavucontrol
    touch checkpoints/003-root.check
 fi
 
-if [ ! -f checkpoints/004-root.check ]
-then
-    echo "   Adding known-good DNS servers..."
-    for i in `ip a | grep -oP "^[0-9]: \K(\w+)(?:)"`;
-    do if [ $i != "lo" ]
-       then sudo systemd-resolve --interface=$i --set-dns=8.8.8.8
-       fi;
-    done
-fi
+# if [ ! -f checkpoints/004-root.check ]
+# then
+#     echo "   Adding known-good DNS servers..."
+#     for i in `ip a | grep -oP "^[0-9]: \K(\w+)(?:)"`;
+#     do if [ $i != "lo" ]
+#        then sudo systemd-resolve --interface=$i --set-dns=8.8.8.8
+#        fi;
+#     done
+#     touch checkpoints/004-root.check
+# fi
 
 loadkeys /home/inaimathi/caps2ctrl.map
